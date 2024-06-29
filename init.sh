97a0186533790dfc46e3a02dbe19ea9c1877ad3a
meinie <meinie@Ubuntu22.04>
2024-06-29 15:14:27 +0800

    init the bashrc of nemu and abs-ma

--- init.sh
+++ init.sh
@@ -12,26 +12,26 @@ function addenv() {
 # usage: init repo branch directory trace [env]
 # trace = true|false
 function init() {
-  if [ -d $3 ]; then
-    echo "$3 is already initialized, skipping..."
-    return
-  fi
+#  if [ -d $3 ]; then
+#    echo "$3 is already initialized, skipping..."
+#    return
+#  fi
 
-  while [ ! -d $3 ]; do
-    git clone -b $2 git@github.com:$1.git $3
-  done
-  log="$1 `cd $3 && git log --oneline --no-abbrev-commit -n1`"$'\n'
+#  while [ ! -d $3 ]; do
+#   git clone -b $2 git@github.com:$1.git $3
+#  done
+#  log="$1 `cd $3 && git log --oneline --no-abbrev-commit -n1`"$'\n'
 
-  if [ $4 == "true" ] ; then
-    rm -rf $3/.git
-    git add -A $3
-    git commit -am "$1 $2 initialized"$'\n\n'"$log"
-  else
-    sed -i -e "/^\/$3/d" .gitignore
-    echo "/$3" >> .gitignore
-    git add -A .gitignore
-    git commit --no-verify --allow-empty -am "$1 $2 initialized without tracing"$'\n\n'"$log"
-  fi
+ # if [ $4 == "true" ] ; then
+  #  rm -rf $3/.git
+   # git add -A $3
+    #git commit -am "$1 $2 initialized"$'\n\n'"$log"
+  #else
+   # sed -i -e "/^\/$3/d" .gitignore
+    #echo "/$3" >> .gitignore
+    #git add -A .gitignore
+    #git commit --no-verify --allow-empty -am "$1 $2 initialized without tracing"$'\n\n'"$log"
+  #fi
 
   if [ $5 ] ; then
     addenv $5 $3
